eb04129a5457b81fce6e14ecb9dbb82b4bf70c93
ki241oav-cmyk <ki241_oav@student.ztu.edu.ua>
2026-04-08 18:19:36 +0300

    Add files via upload

---
+++ LR_2_task_1.py
@@ -0,0 +1,78 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+input_file = 'income_data.txt'
+
+X = []
+y = []
+count_class1 = 0
+count_class2 = 0
+max_datapoints = 25000
+
+with open(input_file, 'r') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+        if '?' in line:
+            continue
+
+        data = line[:-1].split(', ')
+
+        if data[-1] == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+        if data[-1] == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
+
+X = np.array(X)
+
+label_encoder = []
+X_encoded = np.empty(X.shape)
+for i, item in enumerate(X[0]):
+    if item.isdigit():
+        X_encoded[:, i] = X[:, i]
+    else:
+        label_encoder.append(preprocessing.LabelEncoder())
+        X_encoded[:, i] = label_encoder[-1].fit_transform(X[:, i])
+
+X = X_encoded[:, :-1].astype(int)
+y = X_encoded[:, -1].astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+classifier = OneVsOneClassifier(LinearSVC(random_state=0, dual="auto", max_iter=10000))
+
+classifier.fit(X_train, y_train)
+
+y_test_pred = classifier.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_test_pred)
+precision = precision_score(y_test, y_test_pred, average='weighted', zero_division=0)
+recall = recall_score(y_test, y_test_pred, average='weighted', zero_division=0)
+f1 = f1_score(y_test, y_test_pred, average='weighted')
+
+print(f"Accuracy: {round(100 * accuracy, 2)}%")
+print(f"Precision: {round(100 * precision, 2)}%")
+print(f"Recall: {round(100 * recall, 2)}%")
+print(f"F1 score: {round(100 * f1, 2)}%")
+
+input_data = ['37', 'Private', '215646', 'HS-grad', '9', 'Never-married', 'Handlers-cleaners', 'Not-in-family', 'White', 'Male', '0', '0', '40', 'United-States']
+
+input_data_encoded = [-1] * len(input_data)
+count = 0
+for i, item in enumerate(input_data):
+    if item.isdigit():
+        input_data_encoded[i] = int(input_data[i])
+    else:
+        input_data_encoded[i] = int(label_encoder[count].transform([input_data[i]])[0])
+        count += 1
+
+input_data_encoded = np.array([input_data_encoded])
+
+predicted_class = classifier.predict(input_data_encoded)
+print("Результат класифікації:", label_encoder[-1].inverse_transform(predicted_class)[0])
